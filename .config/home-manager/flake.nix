d371aa2ec73b0566f5d52f3c62cbf88a0a80ecda
Ross Kelso <ross@rosskelso.com>
2026-07-28 20:24:42 +0100

    add nix flake with pinned nixpkgs and home-manager

---
+++ .config/home-manager/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Ross's home-manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-26.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+    in {
+      homeConfigurations.rossrkk = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+}
