e45eceefe8669cd291df8688f2b1c59c9807fd14
Ross Kelso <ross@rosskelso.com>
2026-07-30 23:53:24 +0100

    remove nrs alias

--- .config/home-manager/hosts/personal.nix
+++ .config/home-manager/hosts/personal.nix
@@ -9,7 +9,6 @@
 
   home.shellAliases = {
     hms = "home-manager switch --flake ~/.dotfiles/.config/home-manager#rossrkk@personal";
-    nrs = "sudo nixos-rebuild switch --flake ~/prometheus.nix#prometheus";
   };
 
   home.username = "rossrkk";
